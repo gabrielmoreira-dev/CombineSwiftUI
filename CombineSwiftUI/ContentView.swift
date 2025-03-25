b785acbfe8ef74c8ebab33de55e0a7cee1b9caf0
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-03-24 23:55:49 -0300

    feat: Added movie list UI

--- CombineSwiftUI/ContentView.swift
+++ CombineSwiftUI/ContentView.swift
@@ -1,24 +1,55 @@
-//
-//  ContentView.swift
-//  CombineSwiftUI
-//
-//  Created by Gabriel on 24/03/25.
-//
-
+import Combine
 import SwiftUI
 
 struct ContentView: View {
+    private let apiClient: ApiClient
+    @State private var movies = [Movie]()
+    @State private var search = String()
+    @State private var cancellables = Set<AnyCancellable>()
+    private var searchSubject = CurrentValueSubject<String, Never>(String())
+
+    init(apiClient: ApiClient = ApiClient()) {
+        self.apiClient = apiClient
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(movies) { movie in
+            HStack {
+                AsyncImage(url: movie.poster) {
+                    $0.resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 75, height: 75)
+                } placeholder: {
+                    ProgressView()
+                }
+                Text(movie.title)
+            }
         }
-        .padding()
+        .searchable(text: $search)
+        .onAppear {
+            setupSearch()
+        }
+        .onChange(of: search) {
+            searchSubject.send(search)
+        }
+    }
+
+    private func setupSearch() {
+        searchSubject
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .sink { loadMovies(search: $0) }
+            .store(in: &cancellables)
+    }
+
+    private func loadMovies(search: String) {
+        apiClient.fetchMovies(search)
+            .sink { _ in } receiveValue: { movies = $0 }
+            .store(in: &cancellables)
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
