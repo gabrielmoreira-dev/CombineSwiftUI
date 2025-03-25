8e5d76025736199095850ac70d342802ef77f60e
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-03-24 21:08:43 -0300

    Initial Commit

---
+++ CombineSwiftUI/CombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftUIApp.swift
+//  CombineSwiftUI
+//
+//  Created by Gabriel on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
